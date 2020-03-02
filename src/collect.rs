ecb565eb6db18f07facf353b04e7ab86e2536e80
Jun Wu <quark@lihdd.net>
2020-03-02 00:15:27 -0800

    collect: correct unreachable detection
    "unreachable" requires all nodes in a connect component have refcnt 0.

--- src/collect.rs
+++ src/collect.rs
@@ -73,13 +73,14 @@ fn update_refs(list: *mut GcHeader) {
 /// Subtract ref counts in `GcHeader.prev` by calling the non-recursive
 /// `Trace::trace` on every track objects.
 ///
-/// After this, unreachable objects will have ref count down to 0.
+/// After this, potential unreachable objects will have ref count down
+/// to 0. If vertexes in a connected component _all_ have ref count 0,
+/// they are unreachable and can be released.
 fn subtract_refs(list: *mut GcHeader) {
     let mut tracer = |header: &mut GcHeader| {
         if is_collecting(header) {
             debug_assert!(!is_unreachable(header));
-            let prev = header.prev as usize;
-            header.prev = (prev - (1 << PREV_SHIFT)) as _;
+            edit_gc_ref_count(header, -1);
         }
     };
     visit_list(list, |header| {
@@ -87,8 +88,35 @@ fn subtract_refs(list: *mut GcHeader) {
     });
 }
 
+/// Mark objects as reachable recursively. So ref count 0 means unreachable
+/// values. This also removes the COLLECTING flag for reachable objects so
+/// unreachable objects all have the COLLECTING flag set.
+fn mark_reachable(list: *mut GcHeader) {
+    fn revive(header: &mut GcHeader) {
+        // hasn't visited?
+        if is_collecting(header) {
+            unset_collecting(header);
+            if is_unreachable(header) {
+                edit_gc_ref_count(header, 1); // revive
+            }
+            header.value.gc_traverse(&mut revive); // revive recursively
+        }
+    }
+    visit_list(list, |header| {
+        if is_collecting(header) && !is_unreachable(header) {
+            unset_collecting(header);
+            header.value.gc_traverse(&mut revive)
+        }
+    });
+}
+
 /// Release unreachable objects in the linked list.
 fn release_unreachable(list: *mut GcHeader) -> usize {
+    // Mark reachable objects. For example, A refers B. A's gc_ref_count
+    // is 1 while B's gc_ref_count is 0. In this case B should be revived
+    // by A's non-zero gc_ref_count.
+    mark_reachable(list);
+
     let mut count = 0;
 
     // Count unreachable objects. This is an optimization to avoid realloc.
@@ -143,4 +171,14 @@ fn is_unreachable(header: &GcHeader) -> bool {
 fn is_collecting(header: &GcHeader) -> bool {
     let prev = header.prev as usize;
     (prev & PREV_MASK_COLLECTING) != 0
+}
+
+fn unset_collecting(header: &mut GcHeader) {
+    let prev = header.prev as usize;
+    header.prev = ((prev & PREV_MASK_COLLECTING) ^ prev) as _;
+}
+
+fn edit_gc_ref_count(header: &mut GcHeader, delta: isize) {
+    let prev = header.prev as isize;
+    header.prev = (prev + (1 << PREV_SHIFT) * delta) as _;
 }
